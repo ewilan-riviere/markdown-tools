57f50af26ade28654ac73ebbf307d12504bc31a4
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-05-18 21:04:13 +0200

    meta fix

--- types/metadata.d.ts
+++ types/metadata.d.ts
@@ -1,35 +1,3 @@
-declare interface MetadataBase {
-  settings: {
-    robots: string
-    disallow: string
-    color: string
-    locale: string
-    lang: string
-    googleToken: string
-  }
-  website: {
-    title: string
-    titleTemplate: string
-    description: string
-    rating: string
-    keywords: string[]
-    author: string
-    publisher: string
-    copyright: string
-    language: string
-    designer: string
-  }
-  og: {
-    type: string
-    siteName: string
-  }
-  twitter: {
-    creator?: string
-    site?: string
-    url?: string
-  }
-}
-
 declare interface HeadMeta {
   description?: string
   route?: string
@@ -37,20 +5,4 @@ declare interface HeadMeta {
   type?: string
   url?: string
   title?: string
-  articlePublishedTime?: string
-  articleAuthor?: string
-  articleSection?: string
-  bookISBN?: string
-  bookAuthor?: string
-  bookReleaseDate?: string
-  bookTag?: string
-  profileFirstName?: string
-  profileLastName?: string
-}
-
-declare interface HeadElement {
-  hid?: string
-  name?: string
-  property?: string
-  content?: string
 }

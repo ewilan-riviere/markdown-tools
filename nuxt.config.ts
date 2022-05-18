9552a2497eb1d66965a08c25019cb728cfb9dca0
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-05-18 17:50:57 +0200

    add static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   modules: [
     '@nuxtjs/tailwindcss', // https://tailwindcss.nuxtjs.org/setup
     '@pinia/nuxt', // https://pinia.vuejs.org/ssr/nuxt.html

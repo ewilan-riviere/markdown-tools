e9e329815cc4f064882e383a3acfe4cfb64ea69f
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-05-31 12:36:21 +0200

    add home a

--- resources/components/hero.blade.php
+++ resources/components/hero.blade.php
@@ -7,10 +7,12 @@
                 <div class="absolute inset-0 bg-purple-700 mix-blend-multiply"></div>
             </div>
             <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
-                <h1 class="text-center text-4xl font-extrabold tracking-tight md:text-5xl lg:text-6xl">
-                    <span class="block md:inline text-white font-mono mr-2">Markdown</span><span
-                        class="block md:inline text-indigo-200 font-handlee">Scraper</span>
-                </h1>
+                <a href="{{ route('welcome') }}">
+                    <h1 class="text-center text-4xl font-extrabold tracking-tight md:text-5xl lg:text-6xl">
+                        <span class="block md:inline text-white font-mono mr-2">Markdown</span><span
+                            class="block md:inline text-indigo-200 font-handlee">Scraper</span>
+                    </h1>
+                </a>
                 <p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">
                     A tool to extract an article from any website with
                     <a href="https://github.com/FriendsOfPHP/Goutte" target="_blank" rel="noopener noreferrer"
